2b8a75c2413f46e579f0a5ef1e40f27e0566d28d
Sulochana Kodituwakku <sulochana.456@live.com>
2017-02-27 20:57:41 +0530

    Encryption/Decription

--- main.py
+++ main.py
@@ -31,7 +31,26 @@ def getpasswordhash():
     print stamp(), "Prompting user for password"
     pwd = getpass.getpass()
     m.update(pwd)
-    print m.hexdigest()
+    return m.hexdigest()
+
+
+def stringtobinary(txt):
+    out = ""
+    for c in list(txt):
+        tmp = bin(ord(c))[2:]
+        out += "0"*(8-len(tmp))+tmp
+    return out
+
+
+def bytexor(a,b):
+    '''
+    0   0   0
+    0   1   1
+    1   0   1
+    1   1   0
+    '''
+    return a^b
+
 
 def main():
     print stamp(), "Initiating"
@@ -47,7 +66,23 @@ def main():
             print stamp(), "Searching for the file", args.input
             if (os.path.isfile(args.input)):
                 print stamp(), "The file", args.input, "Found"
-                getpasswordhash()
+                password = getpasswordhash()
+                num = 0
+                target=args.input
+                file_op = open(target,"r+b")
+                loop = True
+                while loop:
+                    temp_read = file_op.read(1)
+                    if temp_read != "":
+                        file_op.seek(file_op.tell()-1)
+                        xored = bytexor(ord(temp_read),ord(password[file_op.tell()%len(password)]))
+                        #print xored
+                        #file_op.read(1)
+                        file_op.write(str(chr(xored)))
+                    else:
+                        loop = False
+
+
 
             else:
                 print stamp(2), "The file", args.input, "was not found"
